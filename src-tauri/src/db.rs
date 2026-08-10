4ee317f54e3ba4d3099a03ca2171f53a93097413
ttt_xxx <98574017+TttXxx36@users.noreply.github.com>
2026-08-10 23:00:04 +0800

    fix: restore Rust std import syntax

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -14,7 +14,7 @@ use crate::{
 use rusqlite::{params, Connection, OptionalExtension, Row};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use std{
+use std::{
     collections::{HashMap, HashSet},
     fs,
     path::{Path, PathBuf},
